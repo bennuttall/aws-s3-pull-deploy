82818387925aa5a0cb78c37270f31d15b86e5719
m1ke <git@m1ke.me>
2019-02-11 22:42:00 +0000

    Add examples

---
+++ examples/html/aws-health-check/index.html
@@ -0,0 +1,1 @@
+<h1>AWS Health Check</h1>
---
+++ examples/php/index.php
@@ -0,0 +1,3 @@
+<h1>Pull Deploy Example PHP Site</h1>
+<p>It is <?= date('c')?></p>
+<p>The database endpoint is: <strong><?= file_get_contents('/opt/aws/rds-endpoint')?></strong></p>
